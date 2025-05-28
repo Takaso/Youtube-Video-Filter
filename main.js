aac648b55967ef94f1c3a70d24c5d0aa7b8edaa9
Takaso Nogokui <70639611+Takaso@users.noreply.github.com>
2025-05-28 21:27:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @version      2025-05-28
 // @description  Filter useless videos from youtube
 // @author       Takaso
-// @match        https://www.youtube.com/
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
